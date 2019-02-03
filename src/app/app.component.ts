99221d475440f9a505aa0890f4bfb70499688109
simba0905 <rascalsimba0905@gmail.com>
2019-02-03 16:50:32 +0300

    complete task 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-tour-of-heroes';
+  title = 'Tour of Heroes';
+  topHeros = [1, 2, 3, 4];
+  heroList = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 }
